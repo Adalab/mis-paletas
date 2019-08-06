236b75b6f02325c1965ea9b715360eb639802186
Carlos Mañas <carlos@spacenomads.com>
2019-08-06 20:57:48 +0200

    add 7th step

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,6 +3,36 @@
 const paletteContainer = document.querySelector('.palettes');
 
 const endpoint = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json';
+const favs = [];
+
+function handleFav(event) {
+  const currentPalette = event.currentTarget;
+  const currentPaletteName = currentPalette.getAttribute('data-name');
+
+  currentPalette.classList.toggle('palette--is-favorite');
+  if (currentPalette.classList.contains('palette--is-favorite') === true) {
+    // añade el elemento al array de favoritos
+    if (favs.includes(currentPaletteName) === false) {
+      favs.push(currentPaletteName);
+    }
+  } else {
+    // lo quitamos del array de favoritos
+    const index = favs.indexOf(currentPaletteName);
+    if (index > -1) {
+      favs.splice(index, 1);
+    }
+  }
+  // pintar el array para ver qué ha pasado
+  console.log(favs);
+}
+
+function activateFavs() {
+  const myPalettes = document.querySelectorAll('.palette');
+
+  for (const item of myPalettes) {
+    item.addEventListener('click', handleFav);
+  }
+}
 
 function getPalettes() {
   fetch(endpoint)
@@ -19,7 +49,7 @@ function getPalettes() {
         }
 
         paletteItem += `
-          <li class="palette">
+          <li class="palette" data-name="${item.name}">
             <h2 class="palette__name">${item.name} <small>(${item.from})</small></h2>
             <ul class="palette__colors">
               ${itemColors}
@@ -28,6 +58,8 @@ function getPalettes() {
         `;
       }
       paletteContainer.innerHTML = paletteItem;
+
+      activateFavs();
     });
 }
 
