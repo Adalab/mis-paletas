e11aaab74c67783f50045c8a042b9ada062eeac6
Carlos Mañas <carlos@spacenomads.com>
2019-08-06 18:15:26 +0200

    add 6th step

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,30 +2,32 @@
 
 const paletteContainer = document.querySelector('.palettes');
 
-const endpoint = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palette.json';
+const endpoint = 'https://raw.githubusercontent.com/Adalab/Easley-ejercicios-de-fin-de-semana/master/data/palettes.json';
 
 function getPalettes() {
   fetch(endpoint)
     .then(response => response.json())
     .then(data => {
+      let paletteItem = '';
+      for (const item of data.palettes) {
+        let itemColors = '';
 
-      let itemColors = '';
-      const itemName = data.palettes[0].name;
+        for (const color of item.colors) {
+          itemColors += `
+            <li class="palette__color" style="background-color:#${color}">#${color}</li>
+          `;
+        }
 
-      for (const color of data.palettes[0].colors) {
-        itemColors += `
-          <li class="palette__color" style="background-color:#${color}">#${color}</li>
+        paletteItem += `
+          <li class="palette">
+            <h2 class="palette__name">${item.name} <small>(${item.from})</small></h2>
+            <ul class="palette__colors">
+              ${itemColors}
+            </ul>
+          </li>
         `;
       }
-
-      paletteContainer.innerHTML = `
-        <li class="palette">
-          <h2 class="palette__name">${itemName}</h2>
-          <ul class="palette__colors">
-            ${itemColors}
-          </ul>
-        </li>
-      `;
+      paletteContainer.innerHTML = paletteItem;
     });
 }
 
